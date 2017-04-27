46263e2e85bdf2d7cf518ddd52b86ad09bcbd98e
ivanckt <123123@gmail.com>
2017-04-27 20:46:29 +0800

    MusicPlayer add Graph
    MusicPlayer add Graph

--- ParkYohoKineticControl/src/ofApp.cpp
+++ ParkYohoKineticControl/src/ofApp.cpp
@@ -633,7 +633,7 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    
+	musicPlayer.mousePressed(x, y, button);
 }
 
 //--------------------------------------------------------------
